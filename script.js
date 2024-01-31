8c4d0af98efbc46bccddffb6d38068a3e2f7ed4d
FazekasMarton <fazekas.marton16@gmail.com>
2024-01-31 13:17:29 +0100

    listing function added

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ let numbers = []
 document.getElementById("add").addEventListener("click", () => {
     let number = document.getElementById("number").value
     numbers.push(number)
+    showList()
 })
+
+function showList(){
+    let list = document.getElementById("list")
+    list.innerText = numbers.join(", ")
+}
